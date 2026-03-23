a2acd5dfd95772fefca9c9896e7d12b3a419f2b2
daniel <wanjikumwangidaniel@gmail.com>
2026-03-23 16:48:27 +0300

    modified data-types.js

---
+++ 2-data-types.js
@@ -0,0 +1,32 @@
+//what are data types in JavaScript?
+//Data types are the different types of values that can be stored and manipulated in a programming language. They include:
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+
+// 1. String
+// A string is a sequence of characters enclosed in single or double quotes. It is used to represent text.
+let name = "Alice";
+console.log(name); // Output: Alice
+
+// 2. Number
+// A number can be an integer or a floating-point value. It is used to represent numeric values.
+let age = 25;
+console.log(age); // Output: 25
+
+// 3. Boolean
+// A boolean represents a logical value that can be either true or false. It is used for conditional statements and logical operations.
+let isStudent = true;
+console.log(isStudent); // Output: true
+
+// 4. Undefined
+// Undefined is a data type that represents the absence of a value. It is the default value of uninitialized variables.
+let x;
+console.log(x); // Output: undefined
+
+// 5. Null
+//Null is a data type that represents the intentional absence of any object value. It is often used to indicate that a variable should have no value.
+let y = null;
+console.log(y); // Output: null
