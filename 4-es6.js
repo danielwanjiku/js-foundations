d2d71127ad9da080d62fddbeff41d264b14726a5
daniel <wanjikumwangidaniel@gmail.com>
2026-03-23 17:02:53 +0300

    modified  esg.js

---
+++ 4-es6.js
@@ -0,0 +1,20 @@
+//what is es6 javascript?
+//ES6, also known as ECMAScript 2015, is the sixth major release of the ECMAScript language specification.
+// It introduced several new features and improvements to JavaScript, making it more powerful and easier to work with.
+// Some of the key features of ES6 include:
+
+// 1. let and const: ES6 introduced two new ways to declare variables, let and const.
+//let was intially var.
+let name = "Alice";
+const PI = 3.14;
+console.log(name); // Output: Alice
+console.log(PI); // Output: 3.14
+
+// 2. Arrow Functions: ES6 introduced a new syntax for writing functions called arrow functions.
+const add = (a, b) => a + b;
+console.log(add(5, 3));
+// Output: 8
+
+// 3. Template Literals: ES6 introduced template literals, which allow for easier string interpolation and multi-line strings.
+const greeting = `Hello, ${name}!`;
+console.log(greeting); // Output: Hello, Alice!
