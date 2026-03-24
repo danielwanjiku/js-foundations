49eaffca791416625a6254ab3dee637b4ea828bc
daniel <wanjikumwangidaniel@gmail.com>
2026-03-24 08:34:52 +0300

    modified exercises.js

---
+++ exercises.js
@@ -0,0 +1,14 @@
+// 1. function to calaculate area of rrectangle
+function areaofrectangle(width, height) {
+  return width * height;
+}
+
+//2. function that converts temperatures from celcius to farenheit
+function celciustofarenheit(celcius) {
+  return (celcius * 9) / 5 + 32;
+}
+
+//3. function that calculates factorial of a given number
+function factorialofanumber(n) {
+  return n * factorialofanumber(n - 1);
+}
